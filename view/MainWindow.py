f632ec5a95ff5d9d3897b1d7bb0bcb9b3a2d2cc9
limy <wheesys@sina.com>
2020-01-10 18:52:54 +0800

    init

--- view/MainWindow.py
+++ view/MainWindow.py
@@ -1,25 +1,26 @@
 import sys
 
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtCore import QCoreApplication, QPoint
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QDesktopWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QDesktopWidget, QMainWindow
 
 
-class MainWindow(QWidget):
+class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
         self.init_ui()
 
     def init_ui(self):
-        self.setGeometry(300, 220)
+        self.resize(300, 220)
         self.center()
+
         self.setWindowTitle("OneDrive")
         self.setWindowIcon(QIcon('OneDrive.png'))
 
         close_btn = QPushButton('close', self)
         close_btn.resize(close_btn.sizeHint())
-        close_btn.move(50, 50)
+        close_btn.move(self.width()/2, self.height()/2)
         close_btn.clicked.connect(QCoreApplication.instance().quit)
 
         self.show()
@@ -33,7 +34,7 @@ class MainWindow(QWidget):
 
     def center(self):
         main_window = self.frameGeometry()
-        center_point = QDesktopWidget.availableGeometry(self, QWidget).center()
+        center_point = QApplication.desktop().availableGeometry().center()
         main_window.moveCenter(center_point)
         self.move(main_window.topLeft())
 
